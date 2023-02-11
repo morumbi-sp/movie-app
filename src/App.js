7f876d567e37c17887bff7d2cbf2141f8d88a60b
Morumbi <zun0323@gmail.com>
2023-02-11 10:17:41 -0300

    set router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // {id, name,modified, resourceURI}
 import { useEffect, useState } from 'react';
-import Hero from './Hero';
+import Hero from './components/Hero';
 
 function App() {
   const [loading, setLoading] = useState(true);
