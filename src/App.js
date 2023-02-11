e43bc6cec87c280b4274c81441fca62d616ed33b
Morumbi <zun0323@gmail.com>
2023-02-11 10:20:46 -0300

    move main code from App to Home

--- src/App.js
+++ src/App.js
@@ -3,40 +3,6 @@ import { useEffect, useState } from 'react';
 import Hero from './components/Hero';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const response = await fetch(
-      'https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023'
-    );
-    const json = await response.json();
-    setMovies(json.data.results);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <>
-          <h1>{movies.length} Heros are ready!!</h1>
-          <ul>
-            {movies.map((hero) => (
-              <Hero
-                key={hero.id}
-                name={hero.name}
-                modified={hero.modified}
-                resourceURI={hero.resourceURI}
-              />
-            ))}
-          </ul>
-        </>
-      )}
-    </div>
-  );
+  return null;
 }
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,42 @@
-function Home() {}
+import { useEffect, useState } from 'react';
+import Hero from '../components/Hero';
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      'https://marvel-proxy.nomadcoders.workers.dev/v1/public/characters?limit=50&orderBy=modified&series=24229,1058,2023'
+    );
+    const json = await response.json();
+    setMovies(json.data.results);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <h1>{movies.length} Heros are ready!!</h1>
+          <ul>
+            {movies.map((hero) => (
+              <Hero
+                key={hero.id}
+                name={hero.name}
+                modified={hero.modified}
+                resourceURI={hero.resourceURI}
+              />
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Home;
